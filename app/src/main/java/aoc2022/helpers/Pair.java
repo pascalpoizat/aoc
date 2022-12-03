fc7f4eb00d43e252eee747af10483b35799a1bd8
Pascal Poizat <pascal.poizat@gmail.com>
2022-12-04 00:06:25 +0100

    refactoring

--- app/src/main/java/aoc2022/helpers/Pair.java
+++ app/src/main/java/aoc2022/helpers/Pair.java
@@ -3,27 +3,27 @@ package aoc2022.helpers;
 import java.util.function.BiFunction;
 
 public class Pair<T, U> {
-    private T _1;
-    private U _2;
+    private T fst;
+    private U snd;
 
-    public Pair(T _1, U _2) {
-        this._1 = _1;
-        this._2 = _2;
+    public Pair(T fst, U snd) {
+        this.fst = fst;
+        this.snd = snd;
     }
 
-    public T _1() {
-        return _1;
+    public T fst() {
+        return fst;
     }
 
-    public U _2() {
-        return _2;
+    public U snd() {
+        return snd;
     }
 
     public <V> Pair<V, U> map1(BiFunction<T, U, V> f) {
-        return new Pair<>(f.apply(_1(), _2()), _2());
+        return new Pair<>(f.apply(fst(), snd()), snd());
     }
 
     public <V> Pair<T, V> map2(BiFunction<T, U, V> f) {
-        return new Pair<>(_1(), f.apply(_1(), _2()));
+        return new Pair<>(fst(), f.apply(fst(), snd()));
     }
 }
